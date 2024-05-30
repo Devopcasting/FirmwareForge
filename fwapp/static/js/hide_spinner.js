f5fbb9e1b78ac92b1561281771959de2540a7387
devopcasting <devopcasting@gmail.com>
2024-05-30 12:13:30 +0530

    hide spinner

---
+++ fwapp/static/js/hide_spinner.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // DOM is fully loaded
+    var uploadSpinnerDiv = document.getElementById('uploadSpinnerdiv');
+    uploadSpinnerDiv.style.display = "none";
+   
+});
